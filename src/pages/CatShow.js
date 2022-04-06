3245298f9a3484062eee7863ed7e06468ae0d1ac
Paula Nguyen <nguyen.paula5560@gmail.com>
2022-04-06 16:35:49 -0700

    pushing

--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 
 class CatShow extends Component{
   render(){
-    let { cat } = this.props
     return(
       <>
         <h3>Cat</h3>
-          <img src={cat.image} />
-          <h1>{cat.name}</h1>
-          <p>Age: {cat.age}</p>
-          <p>Enjoys: {cat.enjoys}</p>
+          <img src={this.props.cat && this.props.cat.image} />
+          <h1>{this.props.cat && this.props.cat.name}</h1>
+          <p>Age: {this.props.cat && this.props.cat.age}</p>
+          <p>Enjoys: {this.props.cat && this.props.cat.enjoys}</p>
       </>
     )
   }
