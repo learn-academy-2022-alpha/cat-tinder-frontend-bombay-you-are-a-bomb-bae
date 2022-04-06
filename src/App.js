3ac9ebe69bfc9bf948bec48cef1dddfa87f1bc7c
Jennice <jennicelodi@gmail.com>
2022-04-06 16:23:13 -0700

    added CatIndex and CatShow tests, CatShow still needs work

--- src/App.js
+++ src/App.js
@@ -31,14 +31,23 @@ class App extends Component{
           <Router>
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route path="/catindex" component={CatIndex} />
-              <Route path="/catshow" component={CatShow} />
+              <Route path="/catindex" render={(props) => <CatIndex cats={this.state.cats} />} />
+              <Route 
+                path="/catshow/:id"
+                render={(props) => {
+                  let id = props.match.params.id
+                  let cat = this.state.cats.find( catObj => catObj.id === +id)
+                  console.log(cat)
+                  return <CatShow cat={cat}/>
+                }}
+              />
               <Route path="/catnew" component={CatNew} />
               <Route path="/catedit" component={CatEdit} />
               <Route component={NotFound}/>
             </Switch>
+            <Footer />
           </Router>
-        <Footer />
+
       </>
     )
   }
--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react'
 
 class CatShow extends Component{
   render(){
+    let { cat } = this.props
     return(
       <>
-        <h1>Hi this is the CatShow!</h1>
+        <h3>Cat</h3>
+          <img src={cat.image} />
+          <h1>{cat.name}</h1>
+          <p>Age: {cat.age}</p>
+          <p>Enjoys: {cat.enjoys}</p>
       </>
     )
   }
