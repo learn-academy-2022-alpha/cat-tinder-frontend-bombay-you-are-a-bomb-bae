d14e6737dc8a227e219cfb75e4aeb9e7cfe01df3
Paula Nguyen <nguyen.paula5560@gmail.com>
2022-04-11 09:55:30 -0700

    added the delete functionalit and fetched it. console logs are removed

--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -4,6 +4,26 @@ import { Button } from 'reactstrap'
 
 
 class CatShow extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      newCat: {
+        name: "",
+        age: "",
+        enjoys: "",
+        image: ""
+      },
+      submitted: false
+    }
+  }
+
+handleDelete = () => {
+  // a function call being passed from App.js
+  this.props.deleteCat(this.props.cat.id)
+  this.setState({submitted: true})
+}
+
+
   render(){
     return(
       <>
@@ -14,6 +34,11 @@ class CatShow extends Component{
           <p>Enjoys: {this.props.cat && this.props.cat.enjoys}</p>
           <NavLink to={`/catedit/${this.props.cat && this.props.cat.id}`}>
             <Button>Edit Cat Profile</Button>
+          </NavLink>
+          <NavLink to={`/catindex/${this.props.cat && this.props.cat.id}`}>
+            <Button onClick = {this.handleDelete} >
+              Delete Cat Profile
+            </Button>
           </NavLink>
       </>
     )
